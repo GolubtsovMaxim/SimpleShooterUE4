3750dd123a1daa69ebed0ab3803b471853ba732f
GolubtsovMaxim <maxim.alexandrovich.0409@gmail.com>
2021-04-02 15:51:48 +0300

    init commit

---
+++ Source/SimpleShooterUE4/SimpleShooterUE4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleShooterUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimpleShooterUE4, "SimpleShooterUE4" );
---
+++ Source/SimpleShooterUE4/SimpleShooterUE4GameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "SimpleShooterUE4GameModeBase.h"
+
